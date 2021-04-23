170cc9dd873e622c03fbc5b77ec9d90ae577bd36
sungkyo.lee <sungkyo.lee@lemonhc.com>
2021-04-23 15:18:27 +0900

    jdbc version up, hibernate sql dialect 수정.

--- src/main/java/com/blog/api/vo/Board.java
+++ src/main/java/com/blog/api/vo/Board.java
@@ -7,15 +7,56 @@ import javax.persistence.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity(name="board")
+@Entity
+@Table(name = "board")
 public class Board {
 
+    /**
+     *  게시글 - ID
+     */
     @Id
+    @Column(name = "id", nullable = false)
     private String id;
+
+
+    /**
+     *  게시글 - 제목
+     */
+    @Column(name = "title", nullable = false)
     private String title;
+
+
+    /**
+     *  게시글 - 작성자
+     */
+    @Column(name = "author", nullable = false)
     private String author;
+
+
+    /**
+     *  게시글 - 내용
+     */
+    @Column(name = "contents", nullable = false)
     private String contents;
+
+
+    /**
+     *  게시글 - 조회 수
+     */
+    @Column(name = "view_count", nullable = false)
     private int viewCount;
+
+
+    /**
+     *  게시글 - 조회 수
+     */
+    @Column(name = "create_dtm")
     private String createDtm;
+
+
+    /**
+     *  게시글 - 조회 수
+     */
+    @Column(name = "update_dtm")
     private String updateDtm;
 }
